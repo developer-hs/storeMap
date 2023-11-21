c0d35ed960075b5b97a8f6289821e8998518a6ce
KimHyeongSeop <gygy2006@naver.com>
2023-11-21 23:03:26 +0900

    index.js

--- utils/utils.js
+++ utils/utils.js
@@ -13,13 +13,13 @@ export const removeDuplicates = (arr1, arr2) => {
 };
 
 export const setToken = async (res, user, accessToken, refreshToken) => {
-  res.cookie('access_token', accessToken, {
+  res.cookie('ac', accessToken, {
     httpOnly: true,
     secure: SECURE,
     // 예: secure: true (HTTPS 연결에서만 전송)
   });
 
-  res.cookie('refresh_token', refreshToken, {
+  res.cookie('rt', refreshToken, {
     httpOnly: true,
     secure: SECURE,
     // 다른 쿠키 옵션들 설정
@@ -30,15 +30,15 @@ export const setToken = async (res, user, accessToken, refreshToken) => {
 };
 
 export const setCafe24Token = async (res, user, accessToken, refreshToken) => {
-  user.cafe24RefreshToken = refreshToken;
+  user.cafe24_refresh_token = refreshToken;
 
-  res.cookie('cafe24_access_token', accessToken, {
+  res.cookie('cafe24_ac', accessToken, {
     httpOnly: true,
     secure: true,
     expires: new Date(Date.now() + 7200000), // 2시간
   });
 
-  res.cookie('cafe24_refresh_token', refreshToken, {
+  res.cookie('cafe24_rt', refreshToken, {
     httpOnly: true,
     secure: true,
     expires: new Date(Date.now() + 3600000 * 48), // 2일
